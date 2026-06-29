2f97fcd880541082a9b69931a9df2493a9c07f39
Divyansh Pandey <divyanshpandey062@gmail.com>
2026-06-29 19:02:42 +0530

    Added allocation system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     hostelName: string;
     roomNumber: string;
     capacity: number;
-    hostelType: "Boys" | "Girls";
+    hostelType: "Boys" | "Girls" | string;
   }
 
   interface Participant {
@@ -15,14 +15,17 @@
   }
 
   interface Allocation {
+    id: number;
     participantName: string;
-    gender: "Male" | "Female";
+    gender: "Male" | "Female" | string;
     allottedHostel: string;
     allottedRoom: string;
-}
+  }
 
   let rooms = $state<Room[]>([]);
   let participants = $state<Participant[]>([]);
+
+  let total_capacity = $state(0);
 
   async function handleHostelFile(event: Event) {
     let input = event.target as HTMLInputElement;
@@ -38,6 +41,7 @@
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     rooms = rows.map((row: any) => {
+      total_capacity += row["Capacity"];
       return {
         hostelName: row["Hostel Name"],
         roomNumber: row["Room Number"],
@@ -74,14 +78,53 @@
     });
   }
 
+  let allocations: Allocation[] = $state([]);
 
+  function allocateRooms(
+    participants: Participant[],
+    rooms: Room[],
+  ): Allocation[] {
+    console.log("Function executed");
 
+    let i = 0;
+
+    for (const participant of participants) {
+      i++;
+      let room;
+      let requiredHostel = participant.gender === "Male" ? "Boys" : "Girls";
+      room = rooms.find((room) => {
+        return room.hostelType == requiredHostel && room.capacity > 0;
+      });
+
+      if (!room) {
+        if (!room) {
+          console.log(`No room available for ${participant.name}`);
+          continue;
+        }
+      } else {
+        allocations.push({
+          id: i,
+          participantName: participant.name,
+          gender: participant.gender,
+          allottedHostel: room.hostelName,
+          allottedRoom: room.roomNumber,
+        });
+        room.capacity--;
+        total_capacity--;
+      }
+    }
+    console.log(allocations);
+    return allocations;
+  }
 </script>
 
 <div>
   <div>
     <input onchange={handleHostelFile} type="file" />
     <input onchange={handleParticipantFile} type="file" />
+    <button onclick={() => allocateRooms(participants, rooms)}
+      >Allocate Roms</button
+    >
   </div>
 
   {#each rooms as room (room.roomNumber)}
@@ -92,12 +135,23 @@
       <li>{room.hostelType}</li>
     </div>
   {/each}
-  <br><br>
+  <span>Total Capacity Left: {total_capacity}</span>
+  <br /><br />
 
   {#each participants as participant (participant.id)}
     <div class="flex gap-3 list-none">
       <li>{participant.name}</li>
       <li>{participant.gender}</li>
+    </div>
+  {/each}
+
+  <br><br>
+
+  {#each allocations as allocation (allocation.id)}
+    <div class="flex gap-3 list-none">
+      <li>{allocation.participantName}</li>
+      <li>{allocation.allottedHostel}</li>
+      <li>{allocation.allottedRoom}</li>
     </div>
   {/each}
 </div>
